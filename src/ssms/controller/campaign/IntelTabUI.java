846620808cbd6870e1c0c73ac7775c288d459762
Katie M <mckeanke@msu.edu>
2025-05-20 18:43:14 -0400

    Working on adding inputs to the planets tab

--- src/ssms/controller/campaign/IntelTabUI.java
+++ src/ssms/controller/campaign/IntelTabUI.java
@@ -4,14 +4,17 @@ import com.fs.starfarer.api.Global;
 import com.fs.starfarer.api.campaign.CoreUITabId;
 import com.fs.starfarer.api.campaign.InteractionDialogAPI;
 import com.fs.starfarer.api.campaign.comm.IntelManagerAPI;
+import com.fs.starfarer.api.ui.ButtonAPI;
 import com.fs.starfarer.api.util.Pair;
 import com.fs.starfarer.campaign.CampaignEngine;
 import com.fs.starfarer.campaign.comms.IntelTabData;
-import ssms.controller.Indicators;
-import ssms.controller.InputScreenBase;
-import ssms.controller.InputScreenManager;
+import org.lwjgl.input.Keyboard;
+import ssms.controller.*;
+import ssms.controller.reflection.ClassReflector;
+import ssms.controller.reflection.FieldReflector;
 import ssms.controller.reflection.IntelTabReflector;
 
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,6 +24,10 @@ public class IntelTabUI extends InputScreenBase {
     IntelTabData intelTabData;
     IntelManagerAPI intelManagerAPI;
     List<Pair<Indicators, String>> indicators = null;
+    int lastFrameSelectedIndex = -1;
+    CampaignScope campaignScope;
+    HandlerController controller;
+    int planetSelected = -1;
     @Override
     public String getId() {
         return ID;
@@ -36,6 +43,23 @@ public class IntelTabUI extends InputScreenBase {
         output.add(new Pair<>(Indicators.LeftStick, "Navigate"));
         output.add(new Pair<>(Indicators.LeftTrigger, "Select prev mission"));
         output.add(new Pair<>(Indicators.RightTrigger, "Select next mission"));
+        output.add(new Pair<>(Indicators.B, "Close"));
+        return output;
+    }
+
+    List<Pair<Indicators, String>> getPlanetIndicators() {
+        List<Pair<Indicators, String>> output = new ArrayList<>();
+        output.add(new Pair<>(Indicators.LeftStick, "Navigate"));
+        output.add(new Pair<>(Indicators.A, "Select"));
+        output.add(new Pair<>(Indicators.B, "Close"));
+        return output;
+    }
+
+    List<Pair<Indicators, String>> getFactionIndicators() {
+        List<Pair<Indicators, String>> output = new ArrayList<>();
+        output.add(new Pair<>(Indicators.LeftStick, "Navigate"));
+        output.add(new Pair<>(Indicators.A, "Select"));
+        output.add(new Pair<>(Indicators.B, "Close"));
         return output;
     }
 
@@ -44,6 +68,8 @@ public class IntelTabUI extends InputScreenBase {
         if(indicators == null) {
             indicators = switch (intelTabData.getSelectedTabIndex()) {
                 case 0 -> getIntelIndicators();
+                case 1 -> getPlanetIndicators();
+                case 2 -> getFactionIndicators();
                 default -> new ArrayList<>();
             };
         }
@@ -54,6 +80,49 @@ public class IntelTabUI extends InputScreenBase {
     public void activate(Object ... args) {
         intelTabReflector = (IntelTabReflector) args[0];
         intelTabData = CampaignEngine.getInstance().getUIData().getIntelData();
+        lastFrameSelectedIndex = intelTabData.getSelectedTabIndex();
+        campaignScope = (CampaignScope) InputScreenManager.getInstance().getCurrentScope();
+        controller = (HandlerController) SSMSControllerModPluginEx.controller;
+    }
+
+    void hoverSelectedPlanet() {
+
+    }
+
+    void preInputIntelTab(float amount) {
+
+    }
+
+    void preInputPlanetTab(float amount) {
+        var planets = intelTabReflector.getPlanetsTableRows();
+
+        if(!planets.isEmpty()) {
+            try {
+                if (controller.getButtonEvent(HandlerController.Buttons.LeftStickUp) == 1) {
+                    if (planetSelected == -1 || planetSelected > planets.size()) {
+                        planetSelected = 0;
+                    } else if (planetSelected < (planets.size() - 1)) {
+                        planetSelected++;
+                    }
+
+                    intelTabReflector.ensurePlanetVisible(planets.get(planetSelected));
+                } else if (controller.getButtonEvent(HandlerController.Buttons.LeftStickDown) == 1) {
+                    if (planetSelected == -1 || planetSelected > planets.size()) {
+                        planetSelected = 0;
+                    } else if (planetSelected > 0) {
+                        planetSelected--;
+                    }
+                    intelTabReflector.ensurePlanetVisible(planets.get(planetSelected));
+                }
+            } catch(Throwable ex) {
+                Global.getLogger(getClass()).warn("Error!", ex);
+            }
+            //intelTabReflector.setSelectPlanetTableRow(planets.get(0), null, true);
+        } else planetSelected = -1;
+    }
+
+    void preInputFactionTab(float amount) {
+
     }
 
     @Override
@@ -62,5 +131,19 @@ public class IntelTabUI extends InputScreenBase {
             InputScreenManager.getInstance().transitionDelayed(MainCampaignUI.ID);
             return;
         }
+        switch(intelTabData.getSelectedTabIndex()) {
+            case 0 -> preInputIntelTab(amount);
+            case 1 -> preInputPlanetTab(amount);
+            case 2 -> preInputFactionTab(amount);
+        }
+        if(intelTabData.getSelectedTabIndex() != lastFrameSelectedIndex) {
+            indicators = null;
+            InputScreenManager.getInstance().refreshIndicators();
+        }
+        lastFrameSelectedIndex = intelTabData.getSelectedTabIndex();
+        if(controller.getButtonEvent(HandlerController.Buttons.B) == 1) {
+            InputShim.keyDownUp(Keyboard.KEY_ESCAPE, '\0');
+        }
+        campaignScope.handleInput(amount, true);
     }
 }
--- src/ssms/controller/reflection/IntelTabReflector.java
+++ src/ssms/controller/reflection/IntelTabReflector.java
@@ -2,22 +2,36 @@ package ssms.controller.reflection;
 
 import com.fs.starfarer.api.Global;
 import com.fs.starfarer.api.campaign.CoreUIAPI;
+import com.fs.starfarer.api.ui.ButtonAPI;
 import com.fs.starfarer.api.ui.UIPanelAPI;
 import com.fs.starfarer.campaign.comms.v2.EventsPanel;
+import com.fs.starfarer.campaign.ui.UITable;
 import com.fs.starfarer.campaign.ui.intel.FactionIntelPanel;
 import com.fs.starfarer.campaign.ui.intel.PlanetListV2;
 
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
 import java.lang.invoke.MethodType;
+import java.util.ArrayList;
+import java.util.List;
 
 public class IntelTabReflector {
+    CoreUIAPI coreUIAPI;
     UIPanelAPI intelUiParent;
     EventsPanel eventsPanel;
     UIPanelAPI planetTabData;
     FactionIntelPanel factionIntelPanel;
+
     static MethodHandle getPlanetListV2;
-    private IntelTabReflector(UIPanelAPI intelUiParent, EventsPanel eventsPanel, UIPanelAPI planetTabData, FactionIntelPanel factionIntelPanel) {
+    static MethodHandle getPlanetListTableRows;
+    static MethodHandle selectPlanetTableRow;
+    static Object getList;
+    static Object ensurePlanetVisible;
+    static Class<?> tableItemCls;
+    static Class<?> tableListCls;
+
+    private IntelTabReflector(CoreUIAPI coreUIAPI, UIPanelAPI intelUiParent, EventsPanel eventsPanel, UIPanelAPI planetTabData, FactionIntelPanel factionIntelPanel) {
+        this.coreUIAPI = coreUIAPI;
         this.intelUiParent = intelUiParent;
         this.eventsPanel = eventsPanel;
         this.planetTabData = planetTabData;
@@ -45,6 +59,49 @@ public class IntelTabReflector {
         }
     }
 
+    public void ensurePlanetVisible(Object planetObj) {
+        try {
+            var buttonField = ClassReflector.GetInstance().getDeclaredField(planetObj.getClass().getSuperclass().getSuperclass(), "button");
+
+            var btn = (ButtonAPI) FieldReflector.GetInstance().GetVariable(buttonField, planetObj);
+
+            ensurePlanetVisible(btn);
+            var lst = MethodReflector.GetInstance().invoke(getList, getPlanetList().getTable());
+            MethodReflector.GetInstance().invoke(ensurePlanetVisible, lst, btn);
+        } catch(Throwable ex) {
+            Global.getLogger(getClass()).error("Couldn't ensure that the desired planet is visible in the scroller!");
+        }
+    }
+
+    public void selectPlanetTableRow(Object row, Object arg, boolean b) {
+        try {
+            selectPlanetTableRow.invoke(getPlanetList().getTable(), row, arg, b);
+        } catch(Throwable ex) {
+            Global.getLogger(getClass()).error("Failed to select row of planet list table!");
+        }
+    }
+
+    public List<UIPanelAPI> getPlanetsTableRows() {
+        var planetListUi = getPlanetList();
+        List<UIPanelAPI> planets = new ArrayList<>();
+        if(planetListUi != null)
+        {
+            try {
+                var table = planetListUi.getTable();
+
+                var planetsListRaw = (List<?>) getPlanetListTableRows.invoke(table);
+                for(Object planet : planetsListRaw) {
+                    if(UIPanelAPI.class.isAssignableFrom(planet.getClass())) {
+                        planets.add((UIPanelAPI) planet);
+                    }
+                }
+            } catch(Throwable ex) {
+                Global.getLogger(getClass()).error("Couldn't fetch current list of planets!", ex);
+            }
+        }
+        return planets;
+    }
+
     public static IntelTabReflector TryGet(CoreUIAPI coreUIAPI, BorderedPanelReflector borderedPanelReflector) {
         try {
             var intelTabUi = borderedPanelReflector.getPanel();
@@ -58,9 +115,20 @@ public class IntelTabReflector {
             var getPlanetsPanel = ClassReflector.GetInstance().getDeclaredMethod(intelTabUi.getClass(), "getPlanetsPanel");
             UIPanelAPI planetPanel = (UIPanelAPI) MethodReflector.GetInstance().invoke(getPlanetsPanel, intelTabUi);
 
-            getPlanetListV2 = MethodHandles.lookup().findVirtual(planetPanel, "getPlanetList2", MethodType.methodType(PlanetListV2.class));
+            getPlanetListV2 = MethodHandles.lookup().findVirtual(planetPanel.getClass(), "getPlanetList2", MethodType.methodType(PlanetListV2.class));
 
-            return new IntelTabReflector(intelTabUi, eventsPanel, planetPanel, factionIntelPanel);
+            getPlanetListTableRows = MethodHandles.lookup().findVirtual(UITable.class, "getRows", MethodType.methodType(List.class));
+
+            var getSelected = ClassReflector.GetInstance().getDeclaredMethod(UITable.class, "getSelected");
+            tableItemCls = MethodReflector.GetInstance().getReturnType(getSelected);
+            selectPlanetTableRow = MethodHandles.lookup().findVirtual(UITable.class, "select", MethodType.methodType(void.class, tableItemCls, Object.class, boolean.class));
+
+            getList = ClassReflector.GetInstance().getDeclaredMethod(UITable.class, "getList");
+
+            tableListCls = MethodReflector.GetInstance().getReturnType(getList);
+            ensurePlanetVisible = ClassReflector.GetInstance().findDeclaredMethod(tableListCls, "ensureVisible");
+
+            return new IntelTabReflector(coreUIAPI, intelTabUi, eventsPanel, planetPanel, factionIntelPanel);
         } catch(Throwable ex) {
             Global.getLogger(IntelTabReflector.class).error("Couldn't reflect into IntelTab UI!", ex);
         }
